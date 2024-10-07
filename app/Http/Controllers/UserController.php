e00f0b8350ec1aba1a5b7517b825e0977a41fff3
juliane <watanabejuliane1@gmail.com>
2024-10-07 17:39:09 +0900

    task 02

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,10 @@ class UserController extends Controller
         //   order by created_at desc
         //   limit 3
 
-        $users = User::all(); // replace this with Eloquent statement
+        $users = User::whereNotNull('email_verified_at') // replace this with Eloquent statement
+        ->orderBy('created_at', 'desc')
+        ->take(3)
+        ->get();
 
         return view('users.index', compact('users'));
     }
